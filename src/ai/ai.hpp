9f9b9136664f47b06f1a222e1e48bdf06aea90a2
upsidedown8 <65985223+upsidedown8@users.noreply.github.com>
2020-11-13 18:29:55 +0000

    added ai

---
+++ src/ai/ai.hpp
@@ -0,0 +1,14 @@
+#if !defined(MINESWEEPER_AI_AI_HPP)
+#define MINESWEEPER_AI_AI_HPP
+
+#include "board.hpp"
+
+namespace minesweeper {
+
+class ai {
+    
+};
+
+}
+
+#endif // MINESWEEPER_AI_AI_HPP
